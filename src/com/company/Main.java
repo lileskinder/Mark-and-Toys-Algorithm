065c97d2e45afc93838c2e65b98c8bd34c25433a
lileskinder <33198174+lileskinder@users.noreply.github.com>
2021-08-31 09:49:28 -0500

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+
+    public static int maximumToys(List<Integer> prices, int k) {
+        int sum = 0;
+        int count = 0;
+
+        Collections.sort(prices);
+        for(int i : prices) {
+            if(sum < k) {
+                sum += i;
+                count++;
+            }
+        }
+
+        return count-1;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
